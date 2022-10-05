5850136ea50f4ece545cdd78cffcfc2a402442d6
Obito <vitthalsoni629@gmail.com>
2022-10-05 09:21:40 +0100

    ✅ Added some basic events!

--- Hyper/src/Hyper/Application.cpp
+++ Hyper/src/Hyper/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Events/ApplicationEvent.h"
+#include "Log.h"
+
 namespace Hyper
 {
 
@@ -13,7 +16,19 @@ namespace Hyper
 
 	}
 
-	void Application::Run() {
+	void Application::Run()
+	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			HY_TRACE(e);
+		}
+
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			HY_TRACE(e);
+		}
+
 		while (true);
 	}
 }
